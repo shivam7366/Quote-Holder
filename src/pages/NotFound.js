cec07158b88f9c5c289c86b89fb12668c8f0ada5
Shivam Gupta <87704010+shivam7366@users.noreply.github.com>
2022-03-18 08:38:53 +0530

    code Uploaded

---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <>
+      <div className="centered">Error 404! Page not found🛑🛑🛑🛑.</div>
+    </>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,58 @@
+import { Fragment, useEffect } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+
+const QuoteDetail = () => {
+  const match = useRouteMatch();
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedData,
+    error,
+  } = useHttp(getSingleQuote, true);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (error) {
+    return <div className="centered">{error}</div>;
+  }
+
+  if (!loadedData.text) {
+    return <p>Quote not found.</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedData.text} author={loadedData.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
